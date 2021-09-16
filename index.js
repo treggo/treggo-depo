299bc479eac7082ce03fe63bbe306c04d0f81b4b
Tadeo Rosochacki <taaddde@hotmail.com>
2021-09-16 17:19:00 -0300

    Commit

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ RedisClient.on("connect", async function() {
     console.log("Redis error", JSON.stringify(e));
 })
 
-RedisClient.on("message", function(printer, zpl) {
-  console.log("Subscriber received message in channel '" + printer + "': " + zpl);
+RedisClient.on("message", async function(printer, base64) {
+  console.log("Subscriber received message in channel '" + printer + "': " + base64.slice(0, 10)+"...");
+  const image = new Buffer.from(base64, 'base64');
+  await ptp.print(image, {printer: "depo"});
 });
 
 RedisClient.subscribe("printer_zpl_234_jetsmartj");
