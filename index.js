38753ad58bc921c8d49714b3b3684bb17413c36d
Nicolas Torchio <nicolas.a.torchio@gmail.com>
2021-09-16 18:53:30 -0300

    Fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ RedisClient.on("connect", async function() {
     console.log("Redis error", JSON.stringify(e));
 })
 
-ptp.print("file.pdf", {printer: `"Microsoft XPS Document Writer"`});
+ptp.print("file.pdf", {printer: `"Microsoft XPS Document Writer"`}).then(console.log).catch(console.error);
 
 RedisClient.on("message", async function(printer, base64) {
   console.log("Subscriber received message in channel '" + printer + "': " + base64.slice(0, 10)+"...");
