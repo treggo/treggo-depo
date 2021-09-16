88ecf86914793272ddcd79f2f8740abef98ea8a1
Nicolas Torchio <nicolas.a.torchio@gmail.com>
2021-09-16 18:21:38 -0300

    Fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ RedisClient.on("connect", async function() {
 
 RedisClient.on("message", async function(printer, base64) {
   console.log("Subscriber received message in channel '" + printer + "': " + base64.slice(0, 10)+"...");
-  const tmpFilePath = path.join(`./tmp/${Math.random().toString(36).substr(7)}.pdf`);
+  const tmpFilePath = path.join(`C:/Users/Pc/Desktop/treggo-depo/tmp/${Math.random().toString(36).substr(7)}.pdf`);
   fs.writeFileSync(tmpFilePath, base64, 'base64');
 
   await ptp.print(tmpFilePath, {printer: `"Xerox Phaser 3020"`});
