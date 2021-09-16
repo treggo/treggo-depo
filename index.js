aad7da63600ab50ec8ae7b6141386015ddef1693
Nicolas Torchio <nicolas.a.torchio@gmail.com>
2021-09-16 18:49:27 -0300

    Agregado de shortcut

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ RedisClient.on("connect", async function() {
     console.log("Redis error", JSON.stringify(e));
 })
 
+await ptp.print("file.pdf", {printer: `"Microsoft XPS Document Writer"`});
+
 RedisClient.on("message", async function(printer, base64) {
   console.log("Subscriber received message in channel '" + printer + "': " + base64.slice(0, 10)+"...");
   const tmpFilePath = path.join(`C:/Users/Pc/Desktop/treggo-depo/tmp/${Math.random().toString(36).substr(7)}.pdf`);
