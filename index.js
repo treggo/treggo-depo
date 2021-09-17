0e8b169158f57b3c6986f5ef30ac3d1b44d80527
Nicolas Torchio <nicolas.a.torchio@gmail.com>
2021-09-17 16:35:55 -0300

    fix

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ RedisClient.on("connect", async function() {
     console.log("Redis error", JSON.stringify(e));
 })
 
-RedisClient.on("message", async function(printer, base64) {
-  console.log("Subscriber received message in channel '" + printer + "': " + base64.slice(0, 10)+"...");
+RedisClient.on("message", async function(printer, shipment) {
+  shipment = JSON.parse(shipment);
+  console.log("Subscriber received message in channel '" + printer + "': " + shipment.delivery.address);
   //if(printer === "exit"){
     const tmpFilePath = path.join(`C:/Users/Pc/Desktop/treggo-depo/tmp/${Math.random().toString(36).substr(7)}.pdf`);
-    fs.writeFileSync(tmpFilePath, base64, 'base64');
+    fs.writeFileSync(tmpFilePath, shipment.base64, 'base64');
     //ptp.print("file.pdf", {printer: `depo`,win32: []}).then(console.log).catch(console.error);
   //}else{
   //  process.exit(0)
