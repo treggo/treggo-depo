24622ffeb2e33fc45bee986e5476b747d1ccb168
Nicolas Torchio <nicolas.a.torchio@gmail.com>
2021-09-17 14:49:47 -0300

    Fix

--- index.js
+++ index.js
@@ -6,19 +6,15 @@ const fs = require("fs");
 
 RedisClient.on("connect", async function() {
     console.log("Redis connected");
-    console.log( await ptp.getPrinters())
 }).on('error', (e) => {
     console.log("Redis error", JSON.stringify(e));
 })
-
-ptp.print("file.pdf", {printer: `"Microsoft XPS Document Writer"`,win32: []}).then(console.log).catch(console.error);
 
 RedisClient.on("message", async function(printer, base64) {
   console.log("Subscriber received message in channel '" + printer + "': " + base64.slice(0, 10)+"...");
   const tmpFilePath = path.join(`C:/Users/Pc/Desktop/treggo-depo/tmp/${Math.random().toString(36).substr(7)}.pdf`);
   fs.writeFileSync(tmpFilePath, base64, 'base64');
-
-  await ptp.print(tmpFilePath, {printer: `"Microsoft XPS Document Writer"`});
+  ptp.print("file.pdf", {printer: `depo`,win32: []}).then(console.log).catch(console.error);
 });
 
 RedisClient.subscribe("printer_depo");
