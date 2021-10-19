c947184ced86d71b85fb190796fcfc4b48396ded
Nicolas Torchio <nicolas.a.torchio@gmail.com>
2021-10-19 16:48:02 -0300

    Que no crahsee

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var HID = require('node-hid');
 var devices = HID.devices();
 
-let path = devices[1].path
-var device = new HID.HID(path);
+for (dev of devices) {
+  try {
+    var device = new HID.HID(dev.path);
+    device.on("data", function (data) {
+      console.log("Leyo algo de data", data)
+    });
+    device.on("error", function (err) {
+      console.log(err)
+    });
+  } catch (err) {
 
-for(device of devices){
-  console.log("Leyendo de",path)
-  device.on("data", function(data) {
-    console.log("Leyo algo de data",data)
-  });
-  device.on("error", function(err) {
-    console.log(err)
-  });
+  }
 }
