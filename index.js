8d0b1fa3aee3eca757aa9b60e5e2e27e3c841d1e
Nicolas Torchio <nicolas.a.torchio@gmail.com>
2021-10-19 16:38:48 -0300

    Primer test

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var HID = require('node-hid');
 var devices = HID.devices();
-console.log(devices);
+
+let path = devices[0].path
+var device = new HID.HID(path);
+
+console.log("Leyendo de",path)
+device.on("data", function(data) {
+  console.log("Leyo algo de data",data)
+});
